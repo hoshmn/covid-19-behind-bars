32b6d3689dac30729b91e7e722bb6c5e02e5d466
Lane <laneolson@gmail.com>
2020-08-18 13:55:14 -0600

    add code splitting on production builds

--- packages/national-map/webpack.prod.js
+++ packages/national-map/webpack.prod.js
@@ -2,4 +2,9 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 module.exports = merge(common, {
   mode: "production",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
 });
